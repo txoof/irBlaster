50d02358d0d944f21da605d56ad177e1ca045e1e
txoof <aaron.ciuffo@gmail.com>
2019-03-19 20:26:12 +0100

    improve debugging text, fix power down routine

--- 2channel_audio_sense_rv1.1/2channel_audio_sense_rv1.1.ino
+++ 2channel_audio_sense_rv1.1/2channel_audio_sense_rv1.1.ino
@@ -1,6 +1,7 @@
 #include <elapsedMillis.h>    //measure elapsed time
 #include <movingAvg.h>    // moving average library
-#include <advancedSerial.h>
+#include <advancedSerial.h>   //advanced serial output for better debugging
+//#include
 
 // power on/off
 #define RAW_DATA_LEN 68
@@ -65,6 +66,8 @@ const int audioPin1 = A1;     //Channel 1
 const int statusLight = 10;   //active channel indicator light
 const int audioThreshold = 5; //minimum level to be considered "active"
 const int channelRelease = 5000;     //amount of time to wait before releasing an inactive channel
+const long powerTimeout = 10000;    //amount of time to wait before powering off
+elapsedMillis powerTimer;
 
 
 #define CHANS 2    //define the number of channels to use
@@ -81,7 +84,8 @@ int currentChannel = -1;   //channel that is actively playing over speakers
 int prevChannel = -1;
 
 elapsedMillis channelReleaseTimer;
-//elapsedMillis powerTimer;
+
+elapsedMillis heartBeat;
 
 int findActiveChannel() {   //returns first active channel in the array
   int myChannel = -1;
@@ -100,6 +104,11 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(statusLight, OUTPUT);
 
+  // set all the timers to 0 to start
+  powerTimer = 0;
+  heartBeat = 0;
+  channelReleaseTimer = 0;
+
   Serial.begin(9600);
   delay(2000); while (!Serial); //delay for serial to come online
 
@@ -112,6 +121,7 @@ void setup() {
 
   aSerial.setPrinter(Serial);
   aSerial.setFilter(Level::vvv);
+  aSerial.pln("Audio sensing start");
   /* Uncomment the following line to disable the output. By defalut the ouput is on. */
   // aSerial.off();
 }
@@ -143,45 +153,55 @@ void loop() {
     }
 
     //currentChannel is still active and the audio threshold is high enough, reset the release timer
-//    if (activeChannel == currentChannel and channelValues[activeChannel] >= audioThreshold) {
     if (channelValues[currentChannel] >= audioThreshold) {   //if the current channel is still active, ignore other channels
       channelReleaseTimer = 0;
+      powerTimer = 0;
     }
 
     //activeChannel has changed, check to see if the release timer has expired
-    if (activeChannel != currentChannel) {
-      if (channelReleaseTimer >= channelRelease) {
+    if (activeChannel != currentChannel and channelReleaseTimer > 100) {    //delay 100ms before attempting to capture an inactive channel
+      if (channelReleaseTimer >= channelRelease) {    //if the timer has expired, capture the inactive channel
         aSerial.vv().p("Capturing channel: ").p(currentChannel).p(" due to inactivivity and switching to channel: ").pln(activeChannel);
         currentChannel = activeChannel;
       } else {
-        // this is not quite right - the debug message displays when it is not needed, but it's ok
-        aSerial.vv().p("Releasing inactive channel: ").p(currentChannel).p(" in ").pln(channelRelease - channelReleaseTimer);
+        if (heartBeat >= 500) {
+          aSerial.vv().p("Releasing inactive channel: ").p(currentChannel).p(" in ").p(channelRelease - channelReleaseTimer).pln(" ms");
+        }
       }
     } 
     
-  } else {    //no channel active, attempt to find an active channel or set to -1 (not active)
-    if (channelReleaseTimer >= channelRelease) {
-      currentChannel = findActiveChannel();   //this will return -1 if no channel is active
-    } else {
-      //log the inactivity countdown
-      //figure out how to use the power time out here
-      aSerial.vv().p("No channel active; releasing in ").pln(channelRelease - channelReleaseTimer);
+  } else {    //no channel active - set active channel to < 0
+//    if (channelReleaseTimer >= channelRelease) {
+    if (powerTimer >= powerTimeout) {
+      currentChannel = findActiveChannel();   //this will return -1 if no channel is active1
     }
   }
+  
 
-  //action - if channel change is detected, take appropriate action
-  if (currentChannel != prevChannel) {
-    digitalWrite(statusLight, channelIsActive);
-    aSerial.v().p("Channel changed from: ").p(prevChannel).p(" to: ").pln(currentChannel);
-    aSerial.v().pln("Sendinging power-on signal");
-    prevChannel = currentChannel;
+  if (currentChannel != prevChannel) {    //if a channel change happened send appropriate signals
+    
+    if (currentChannel > -1) {    //change the channel if the change was to an active source
+      aSerial.v().p("Channel changed from: ").p(prevChannel).p(" to: ").pln(currentChannel);
+      // change channels here 
+    } else {
+      aSerial.v().p("Input sources became inactive and changed from: ").p(prevChannel).p(" to: ").pln(currentChannel);
+    }
 
-    //current channel is not in off state - send appropriate channel signal
-    if (currentChannel > -1) {
-      aSerial.vv().p("Switching to source: ").pln(sourcesSTR[currentChannel]);
-    }   //end of switch source
+    if ((currentChannel > -1 and prevChannel < 0) or (currentChannel < 0 and prevChannel > -1)) {
+      aSerial.v().p("Setting power-on state to: ").pln(channelIsActive);
+      digitalWrite(statusLight, channelIsActive);
+    }
+
+    prevChannel = currentChannel;   //record the channel change
   }
 
+
+  
+
+  if (heartBeat >= 500) {
+    
+    heartBeat = 0;    //reset heartbeat
+  }
 
   delay(2);
 }
